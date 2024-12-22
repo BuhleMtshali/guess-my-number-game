05e6c270c6f562fcda2af552e640d34663abc1a5
Buhle <mtshalizanobuhle7@gmail.com>
2024-12-22 14:02:31 +0200

    updated the js file and defined the variables

---
+++ index.js
@@ -0,0 +1,11 @@
+//defining variables in the global scope
+let againBtn = document.getElementById('again-btn');
+let randomNumber = document.getElementById('random-number');
+let guessedNumber = document.getElementById('number-guessed');
+let checkBtn = document.getElementById('check-btn');
+let updatingText = document.getElementById('update-text');
+let scoreUpdate = document.getElementById('score-update');
+let highScoreText = document.getElementById('high-score');
+let modalContainer = document.getElementById('modal');
+let closeModalBtn = document.getElementById('close-modal');
+let overlayElement = document.querySelector('.overlay');
